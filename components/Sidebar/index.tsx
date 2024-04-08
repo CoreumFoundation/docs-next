626559c8bece69f21ff76ec26c7451754d4e9d10
Oleksandr Khlopiachyi <akhlopiachyi@gmail.com>
2024-04-08 14:57:03 +0300

    fix: update sidebar

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -6,31 +6,36 @@ import { ChevronRightIcon } from '@heroicons/react/24/outline';
 import { NavigationItem } from '@/utils/types';
 import { SIDEBAR_ITEMS } from './constants';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 interface SidebarProps {
   children: React.ReactNode;
 }
 
-const renderLink = (item: NavigationItem) => {
+const renderLink = (item: NavigationItem, isActive: boolean) => {
   if (item.external) {
     return (
       <a
         href={item.href}
         className={classNames(
-          'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold text-white capitalize'
+          'group flex items-center gap-x-3 rounded-md p-2 text-sm leading-6 text-[#5E6773] font-normal hover:text-white'
         )}
         target='blank'
       >
         {item.name}
+        <svg className="group-hover:stroke-white" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fillRule="evenodd" clipRule="evenodd" d="M14.492 9.46915H8.43281V8H17.0029V16.5701H15.5337V10.5051L8.03885 18L7 16.9612L14.492 9.46915Z" fill="#5E6773"/>
+        </svg>
       </a>
-    );
+    );q
   }
 
   return (
     <Link
       href={item.href}
       className={classNames(
-        'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold text-white capitalize'
+        isActive ? 'text-[#25D695] font-medium bg-sidebar-active' : 'text-[#5E6773] font-normal hover:text-white',
+        'group flex gap-x-3 rounded-md p-2 text-sm leading-6'
       )}
     >
       {item.name}
@@ -38,38 +43,46 @@ const renderLink = (item: NavigationItem) => {
   );
 };
 
-const renderNavigationItems = (items: NavigationItem[]) => {
+const renderNavigationItems = (items: NavigationItem[], pathname: string) => {
   if (!items) return null;
 
-  return items.map((item: NavigationItem) => (
-    <li key={item.name}>
-      {!item.children?.length ? renderLink(item) : (
-        <Disclosure>
-          {({ open }) => (
-            <>
-              <Disclosure.Button
-                className={classNames(
-                  open ? 'bg-[#17191E] text-[#eeeeee]' : 'text-[#868991]',
-                  'flex items-center w-full rounded-lg p-3 pr-2 gap-x-6 text-base font-normal text-[#868991] capitalize'
-                )}
-              >
-                {item.name}
-                <ChevronRightIcon
-                  className={`w-3 h-3 ml-auto ${open ? 'transform rotate-90' : 'transform rotate-0'}`}
-                />
-              </Disclosure.Button>
-              <Disclosure.Panel as="ul" className="my-2 ml-4 px-2 border-l border-l-1 border-[#17191E]">
-                {renderNavigationItems(item.children || [])}
-              </Disclosure.Panel>
-            </>
-          )}
-        </Disclosure>
-      )}
-    </li>
-  ));
+  return items.map((item: NavigationItem) => {
+    const isActive = pathname === item.href;
+
+    return (
+      <li key={item.name}>
+        {!item.children?.length ? renderLink(item, isActive) : (
+          <Disclosure>
+            {({ open }) => (
+              <>
+                <Disclosure.Button
+                  className={classNames(
+                    open ? 'bg-[#17191E] text-[#eeeeee]' : 'text-[#868991]',
+                    isActive ? 'text-red-700' : '',
+                    'flex items-center w-full rounded-lg p-3 pr-2 gap-x-6 text-base font-normal text-[#868991] capitalize'
+                  )}
+                >
+                  {item.name}
+                  <ChevronRightIcon
+                    className={`
+                    w-3 h-3 ml-auto ${open ? 'transform rotate-90 text-[#25D695]' : 'transform rotate-0 text-[#868991]'}`}
+                  />
+                </Disclosure.Button>
+                <Disclosure.Panel as="ul" className="my-2 ml-4 px-2 border-l border-l-1 border-[#17191E]">
+                  {renderNavigationItems(item.children || [], pathname)}
+                </Disclosure.Panel>
+              </>
+            )}
+          </Disclosure>
+        )}
+      </li>
+    );
+  });
 };
 
 export const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+  const pathname = usePathname();
+
   return (
     <div className="flex flex-row w-full h-auto min-h-full flex-1 font-['space grotesk'] ">
       <div className="flex flex-none w-72">
@@ -78,7 +91,7 @@ export const Sidebar: React.FC<SidebarProps> = ({ children }) => {
             <ul role="list" className="flex flex-1 flex-col gap-y-7">
               <li>
                 <ul role="list" className="-mx-2 space-y-1">
-                  {renderNavigationItems(SIDEBAR_ITEMS)}
+                  {renderNavigationItems(SIDEBAR_ITEMS, pathname)}
                 </ul>
               </li>
             </ul>
