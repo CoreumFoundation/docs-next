0344e4202ec3913564af8e101fac0bde029dafcd
Jonathan Lourie <65506981+jonlourie@users.noreply.github.com>
2024-04-13 03:21:42 -0400

    fixed xrpl page and rest of md links (#10)

--- app/docs/overview/xrpl-bridge/page.tsx
+++ app/docs/overview/xrpl-bridge/page.tsx
@@ -1,9 +1,19 @@
 'use client';
+import Component from './xrpl-bridge.mdx';
+import { NavigatonItem, NavigatonItemMode } from '@/components/NavigationItem';
 
 const XRPLBridgeMainPage = () => {
+  const prevNavigationItem = {
+    label: 'Tools/Ecosystem',
+    href: '/docs/overview/smart-contracts',
+  };
+
   return (
-    <div>
-      <h1>XRPL Bridge main page</h1>
+    <div className="flex flex-col w-full">
+      <Component />
+      <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-10 py-10">
+        <NavigatonItem href={prevNavigationItem.href} label={prevNavigationItem.label} mode={NavigatonItemMode.Previous} />
+      </div>
     </div>
   );
 };
