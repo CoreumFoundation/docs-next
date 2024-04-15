71454ef42c32427222eb4b1a2d99f4fc9995a3af
Oleksandr Khlopiachyi <akhlopiachyi@gmail.com>
2024-04-15 14:14:22 +0300

    feat: add transition effect for main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { CardImage } from "@/components/CardImage";
+import Transition from "@/components/Transition";
 import Image from "next/image";
 import Link from 'next/link';
 
@@ -33,58 +34,60 @@ const CARDS: CardItem[] = [
 
 export default function Home() {
   return (
-    <div className="flex min-h-max flex-col items-center justify-between px-6 pt-2 pb-20 md:px-14 lg:px-20 xl:px-32 w-full gap-20 max-w-7xl">
-      <div className="flex flex-col md:flex-row w-full mb-10">
-        <div className="flex flex-col w-full md:w-3/5 gap-8 md:max-w-[70%] py-14 md:py-16 xl:py-24">
-          <div className="flex text-[#5E6773] text-xs">
-            <div className="flex py-1 px-4 bg-grey-gradient border border-[#5e6773]/25 rounded-full backdrop-blur-sm">
-              Last Updated: 12/14/2023, 6:39:40 AM
+    <Transition>
+      <div className="flex min-h-max flex-col items-center justify-between px-6 pt-2 pb-20 md:px-14 lg:px-20 xl:px-32 w-full gap-20 max-w-7xl">
+        <div className="flex flex-col md:flex-row w-full mb-10">
+          <div className="flex flex-col w-full md:w-3/5 gap-8 md:max-w-[70%] py-14 md:py-16 xl:py-24">
+            <div className="flex text-[#5E6773] text-xs">
+              <div className="flex py-1 px-4 bg-grey-gradient border border-[#5e6773]/25 rounded-full backdrop-blur-sm">
+                Last Updated: 12/14/2023, 6:39:40 AM
+              </div>
+            </div>
+            <div className="flex flex-col text-[4rem] font-bold leading-tight">
+              The World’s First <span className="text-green-gradient">Super Ledger</span>
+              <div className="text-xl text-[#9FA2AC] font-normal leading-[1.75]">
+                Coreum is a scalable, secure, and interoperable blockchain for business, enriched with smart tokens and smart contract functionalities.
+              </div>
+            </div>
+            <div className="flex">
+              <Link href="/docs" className="flex items-center py-2 pl-6 pr-5 rounded-lg text-[#1B1D23] text-sm bg-green-gradient">
+                Get Started
+                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M12.8434 13.0714V8.19407L6.72266 14.3148L5.68018 13.2723L11.7985 7.15398H6.92596V5.67969H14.3177V13.0714H12.8434Z"
+                    fill="#1B1D23"
+                  />
+                </svg>
+              </Link>
             </div>
           </div>
-          <div className="flex flex-col text-[4rem] font-bold leading-tight">
-            The World’s First <span className="text-green-gradient">Super Ledger</span>
-            <div className="text-xl text-[#9FA2AC] font-normal leading-[1.75]">
-              Coreum is a scalable, secure, and interoperable blockchain for business, enriched with smart tokens and smart contract functionalities.
-            </div>
-          </div>
-          <div className="flex">
-            <Link href="/docs" className="flex items-center py-2 pl-6 pr-5 rounded-lg text-[#1B1D23] text-sm bg-green-gradient">
-              Get Started
-              <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M12.8434 13.0714V8.19407L6.72266 14.3148L5.68018 13.2723L11.7985 7.15398H6.92596V5.67969H14.3177V13.0714H12.8434Z"
-                  fill="#1B1D23"
-                />
-              </svg>
-            </Link>
+          <div className="flex w-full md:w-2/5 justify-center md:justify-end">
+            <Image className="h-auto" src="/images/chains.png" alt="chains" width={300} height={420} />
           </div>
         </div>
-        <div className="flex w-full md:w-2/5 justify-center md:justify-end">
-          <Image className="h-auto" src="/images/chains.png" alt="chains" width={300} height={420} />
+        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-3">
+          {CARDS.map((cardItem: CardItem, index: number) => {
+            return (
+              <div
+                className="flex flex-col w-full p-6 card gap-2"
+                key={`${cardItem.title}-${index}`}
+              >
+                <div className="flex w-full mb-4">
+                  {cardItem.image}
+                </div>
+                <div className="text-[#eee] text-base font-medium">
+                  {cardItem.title}
+                </div>
+                <div className="text-[#9FA2AC] text-sm font-normal">
+                  {cardItem.content}
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-3">
-        {CARDS.map((cardItem: CardItem, index: number) => {
-          return (
-            <div
-              className="flex flex-col w-full p-6 card gap-2"
-              key={`${cardItem.title}-${index}`}
-            >
-              <div className="flex w-full mb-4">
-                {cardItem.image}
-              </div>
-              <div className="text-[#eee] text-base font-medium">
-                {cardItem.title}
-              </div>
-              <div className="text-[#9FA2AC] text-sm font-normal">
-                {cardItem.content}
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </div>
+    </Transition>
   );
 }
