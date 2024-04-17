2b49c94675f2ab34e340094b6e0fc2cf5d32e36e
jonlourie <jonlourie@gmail.com>
2024-04-16 21:48:33 -0400

    fixed button colors links and seo

--- app/docs/overview/xrpl-bridge/page.tsx
+++ app/docs/overview/xrpl-bridge/page.tsx
@@ -1,6 +1,32 @@
 'use client';
 import Component from './xrpl-bridge.mdx';
 import { NavigatonItem, NavigatonItemMode } from '@/components/NavigationItem';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "XRPL-Coreum Bridge: Enabling Blockchain Interoperability | Coreum Developers",
+  description: "Discover the XRPL-Coreum Bridge, a groundbreaking solution for seamless asset transfers between XRPL and IBC-compatible appchains. Learn about the bridge's decentralized, non-custodial mechanism and its architecture involving smart contracts and multi-signing accounts.",
+  keywords: [
+    'XRPL-Coreum Bridge',
+    'Blockchain Interoperability',
+    'Asset Transfer Blockchain',
+    'Decentralized Blockchain Mechanism',
+    'Multi-signing Blockchain Account',
+    'Blockchain Smart Contracts',
+    'Coreum Blockchain Solutions',
+    'XRPL Token Bridging',
+    'Blockchain Relayers',
+    'Smart Contract Minting Tokens'
+  ],
+  openGraph: {
+    type: 'website',
+    description: "Explore the XRPL-Coreum Bridge that facilitates asset transfers between the XRPL and Coreum blockchains, enhancing blockchain interoperability with a robust, decentralized architecture.",
+    siteName: "XRPL-Coreum Bridge: Enabling Blockchain Interoperability | Coreum Developers",
+    images: [{
+      url: 'https://test.docs.coreum.dev/images/og.jpg',
+    }],
+  },
+};
 
 const XRPLBridgeMainPage = () => {
   const prevNavigationItem = {
