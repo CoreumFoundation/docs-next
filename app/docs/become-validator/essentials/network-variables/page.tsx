30efa3861ea0a49f9911fc4198fa89974dd534a5
Jahanzeb Hussain <jahanzebhussain@jahanzebslaptop.home>
2024-07-08 12:34:38 -0400

    testing links

--- app/docs/become-validator/essentials/network-variables/page.tsx
+++ app/docs/become-validator/essentials/network-variables/page.tsx
@@ -1,24 +1,26 @@
-
 import Component from './network-variables.mdx';
+import { NavigatonItem, NavigatonItemMode } from '@/components/NavigationItem';
 
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "Setting Up Network Variables for Coreum Environments | Coreum Docs",
-  description: "Learn how to configure network variables for Coreum's Mainnet, Testnet, Devnet, and Znet environments. Essential steps for developers aiming to connect to various Coreum networks.",
+  title: "Install Coreum's Core Daemon (cored): Comprehensive Guide | Coreum Docs",
+  description: "Explore detailed instructions on installing Coreum's core daemon (cored) through various methods including cosmovisor for Linux, prebuilt binaries, and building from sources.",
   keywords: [
-    'Coreum network setup',
-    'Blockchain environment variables',
-    'Coreum Mainnet configuration',
-    'Testnet settings',
-    'Devnet setup',
-    'Coreum blockchain development',
-    'Network variables guide'
+    'Coreum Validator setup',
+    'CLI network variables',
+    'Coreum mainnet',
+    'Coreum testnet',
+    'Coreum devnet',
+    'blockchain CLI configuration',
+    'Coreum RPC nodes',
+    'Coreum REST API',
+    'Coreum network environments'
   ],
   openGraph: {
     type: 'website',
-    description: "Learn how to configure network variables for Coreum's Mainnet, Testnet, Devnet, and Znet environments. Essential steps for developers aiming to connect to various Coreum networks.",
-    siteName: "Setting Up Network Variables for Coreum Environments | Coreum Docs",
+    description: "Step-by-step guide on setting up Coreum CLI Validator network variables for mainnet, testnet, devnet, and localnet environments. Learn how to configure your CLI for Coreum blockchain interactions.",
+    siteName: "Setting Up Coreum Validator Network Variables for Different Environments | Coreum Docs",
     images: [{
       url: 'https://test.docs.coreum.dev/images/og.jpg',
     }],
@@ -27,9 +29,24 @@ export const metadata: Metadata = {
 
 
 const Page = () => {
-  return (
-    <Component />
-  );
-};
-
-export default Page;
+    const prevNavigationItem = {
+      label: 'Set Connnection config',
+      href: '/docs/become-validator/essentials/connection-config',
+    };
+    const nextNavigationItem = {
+      label: 'Troubleshooting',
+      href: '/docs/become-validator/troubleshooting/',
+    };
+  
+    return (
+      <div className="flex flex-col w-full">
+        <Component />
+        <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-10 py-10">
+          <NavigatonItem href={prevNavigationItem.href} label={prevNavigationItem.label} mode={NavigatonItemMode.Previous} />
+          <NavigatonItem href={nextNavigationItem.href} label={nextNavigationItem.label} mode={NavigatonItemMode.Next} />
+        </div>
+      </div>
+    );
+  };
+  
+  export default Page;
